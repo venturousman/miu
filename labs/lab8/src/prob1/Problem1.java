f3f6f60e6c5eb24404d6e3ebf3ee4aa0ae7aa111
nguyenhoangtin291 <nguyenhoangtin291@gmail.com>
2024-09-17 14:07:17 -0500

    commit solution for problem1

--- labs/lab8/src/prob1/Problem1.java
+++ labs/lab8/src/prob1/Problem1.java
@@ -9,38 +9,45 @@ import java.util.function.Predicate;
 
 public class Problem1 {
 
+	// https://docs.oracle.com/en/java/javase/17/docs/api/java.base/java/util/function/package-summary.html
+	// Problem 1: Identify the suitable functional Interface from the Java API and
+	// complete code for the given tasks.
+
 	public static void main(String[] args) {
 		// To understand functions as First class citizen
 		List<Integer> numbers = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5));
 		// Task 1: Identify the suitable interface to read the input and print doubled
-		changeDouble(numbers /* , Functional Interface implementation */ );// Output: 2 4 6 8 10
+		Consumer<Integer> action = (x) -> System.out.println(x * x);
+		changeDouble(numbers, action);// Output: 2 4 6 8 10
 
 		List<String> data = Arrays.asList("apple", "banana", "cherry");
 		// Task 2: Take input of String and transform into Upper case
-		transformStrings(data /* , Functional Interface implementation */);
+		Function<String, String> transformer = (s) -> s.toUpperCase();
+		transformStrings(data, transformer);
 
 		// Task 3: Check the inputs of given value is divisible by 5.
 		List<Integer> inputs = Arrays.asList(10, 21, 12, 25, 33);
-		printIf(inputs /* , Functional Interface implementation */);
+		Predicate<Integer> condition = (x) -> x % 5 == 0;
+		printIf(inputs, condition);
 	}
 
-	public static void changeDouble(List<Integer> numbers /* ,Functional Interface action */) {
+	public static void changeDouble(List<Integer> numbers, Consumer<Integer> action) {
 		for (Integer number : numbers) {
-			// action.accept(number);
+			action.accept(number);
 		}
 	}
 
-	public static void transformStrings(List<String> list/* ,Functional Interface transformer */) {
+	public static void transformStrings(List<String> list, Function<String, String> transformer) {
 		for (String s : list) {
-			// System.out.println(transformer.apply(s));
+			System.out.println(transformer.apply(s));
 		}
 	}
 
-	public static void printIf(List<Integer> numbers/* , transformer condition */) {
+	public static void printIf(List<Integer> numbers, Predicate<Integer> condition) {
 		for (Integer number : numbers) {
-			/*
-			 * if (condition.test(number)) { System.out.println(number); }
-			 */
+			if (condition.test(number)) {
+				System.out.println(number);
+			}
 		}
 	}
 }
