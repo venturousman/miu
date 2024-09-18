d21d181113e00c1fe20dce81484bd02bf29b6668
nguyenhoangtin291 <nguyenhoangtin291@gmail.com>
2024-09-18 16:24:17 -0500

    commit solution for problem 5

--- labs/lab9/src/prob5/Section.java
+++ labs/lab9/src/prob5/Section.java
@@ -6,7 +6,10 @@ import java.util.stream.Stream;
 
 public class Section {
 	public static Stream<String> streamSection(Stream<String> stream, int m, int n) {
-		return null; // implement
+		return stream.skip(m).limit(n - m + 1); // implement
+		// extracts a substream from the input stream 'stream' consisting of all elements
+		// from position m to position n, inclusive; you must use only functional style
+		// of coding. You can assume 0 <= m <= n.
 	}
 
 	public static void main(String[] args) {
@@ -15,7 +18,10 @@ public class Section {
 		 * for the m and n. Use nextStream() method to supply the Stream input as a
 		 * first argument in streamSection() method
 		 */
-
+		// streamSection(nextStream(), 0, 3). print the result as [aaa, bbb, ccc, ddd]
+		System.out.println(streamSection(nextStream(), 0, 3).toList());
+		System.out.println(streamSection(nextStream(), 2, 8).toList());
+		System.out.println(streamSection(nextStream(), 1, 5).toList());
 	}
 
 	// support method for the main method -- for testing
