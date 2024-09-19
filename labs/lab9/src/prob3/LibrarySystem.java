ffc05b390e299dfd2e56ff64846638a0269081db
nguyenhoangtin291 <nguyenhoangtin291@gmail.com>
2024-09-18 22:03:24 -0500

    commit solution for problem 3

--- labs/lab9/src/prob3/LibrarySystem.java
+++ labs/lab9/src/prob3/LibrarySystem.java
@@ -1,5 +1,6 @@
 package prob3;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -14,12 +15,17 @@ public class LibrarySystem {
 				List.of("Becoming", "Leadership", "Creativity", "Sapiens", "Rebecca"));
 
 		// Task 1. Create a list of libraries from the given three Library objects
+		List<Library> list = Arrays.asList(library1, library2, library3);
 
 		// Task 2: Filter libraries that have more than 3 books and print the name of
 		// the Library
+//		list.stream().filter(x -> x.getBooks().size() > 3).forEach(x -> System.out.println(x.getName()));
+		String names = list.stream().filter(x -> x.getBooks().size() > 3).map(Library::getName).collect(Collectors.joining(", "));
+		System.out.println("Task 2: Filter libraries that have more than 3 books: " + names);
 
 		// Task 3: FlatMap to get all the book titles from the libraries and print the
 		// results on the console
-
+		String books = list.stream().flatMap(x -> x.getBooks().stream()).collect(Collectors.joining(", "));
+		System.out.println("Task 3: FlatMap to get all the book titles from the libraries: " + books);
 	}
 }
