25df0445818efdb8ed942898f8b7b97107efd677
nguyenhoangtin291 <nguyenhoangtin291@gmail.com>
2024-09-23 15:11:13 -0500

    commit solution for problem 3

--- labs/lab10/src/prob3/Solution.java
+++ labs/lab10/src/prob3/Solution.java
@@ -25,10 +25,29 @@ public class Solution {
 		return false;
 	}
 
+	public static <T> boolean contains2(List<T> list, T s) {
+		for (T x : list) {
+			if (x == null && s == null)
+				return true;
+			if (x == null || s == null)
+				continue;
+			if (x.equals(s))
+				return true;
+		}
+		return false;
+	}
+
 	public static void main(String[] args) {
 		List<String> list = Arrays.asList("Bob", "Joe", "Tom");
 		boolean result = Solution.contains1(list, "Tom");
 		System.out.println(result);
+
+		boolean result2 = Solution.contains2(list, "Tom");
+		System.out.println(result2);
+
+		List<Integer> intList = Arrays.asList(5, 2, 4, 1, 7);
+		boolean result3 = Solution.contains2(intList, 1);
+		System.out.println(result3);
 	}
 
 }
